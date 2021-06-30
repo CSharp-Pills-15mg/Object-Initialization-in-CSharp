8ed965b09ab64522bffb24e6b9183c1383c13eeb
Iuga Alexandru <coldblackstar@yahoo.com>
2021-06-30 19:59:35 +0300

    added more fields and properties. renamed the Trace class to Dummy.

--- sources/ObjectInitialization/Base.cs
+++ sources/ObjectInitialization/Base.cs
@@ -4,16 +4,30 @@ namespace ObjectInitialization
 {
     internal class Base
     {
-        private static Trace staticField = new Trace("Base.Static.Field");
-        private Trace instanceField = new Trace("Base.Instance.Field");
+        // Static 
 
-        private static Trace StaticProperty { get; set; } = new Trace("Base.Static.Property");
-        private Trace InstanceProperty { get; set; } = new Trace("Base.Instance.Property");
+        private static Dummy staticField1 = new Dummy("Base.Static.Field1");
+
+        public static Dummy StaticProperty1 { get; set; } = new Dummy("Base.Static.Property1");
+
+        private static Dummy staticField2 = new Dummy("Base.Static.Field2");
+
+        public static Dummy StaticProperty2 { get; set; } = new Dummy("Base.Static.Property2");
 
         static Base()
         {
             Console.WriteLine("Base.Static.Constructor");
         }
+
+        // Instance
+
+        private Dummy instanceField1 = new Dummy("Base.Instance.Field1");
+
+        public Dummy InstanceProperty1 { get; set; } = new Dummy("Base.Instance.Property1");
+
+        private Dummy instanceField2 = new Dummy("Base.Instance.Field2");
+
+        public Dummy InstanceProperty2 { get; set; } = new Dummy("Base.Instance.Property2");
 
         public Base()
         {
--- sources/ObjectInitialization/Derived.cs
+++ sources/ObjectInitialization/Derived.cs
@@ -4,16 +4,30 @@ namespace ObjectInitialization
 {
     internal class Derived : Base
     {
-        private static Trace staticField = new Trace("Derived.Static.Field");
-        private Trace instanceField = new Trace("Derived.Instance.Field");
+        // Static 
 
-        private static Trace StaticProperty { get; set; } = new Trace("Derived.Static.Property");
-        private Trace InstanceProperty { get; set; } = new Trace("Derived.Instance.Property");
+        private static Dummy staticField1 = new Dummy("Derived.Static.Field1");
+
+        public static Dummy StaticProperty1 { get; set; } = new Dummy("Derived.Static.Property1");
+
+        private static Dummy staticField2 = new Dummy("Derived.Static.Field2");
+
+        public static Dummy StaticProperty2 { get; set; } = new Dummy("Derived.Static.Property2");
 
         static Derived()
         {
             Console.WriteLine("Derived.Static.Constructor");
         }
+
+        // Instance
+
+        private Dummy instanceField1 = new Dummy("Derived.Instance.Field1");
+
+        public Dummy InstanceProperty1 { get; set; } = new Dummy("Derived.Instance.Property1");
+
+        private Dummy instanceField2 = new Dummy("Derived.Instance.Field2");
+
+        public Dummy InstanceProperty2 { get; set; } = new Dummy("Derived.Instance.Property2");
 
         public Derived()
         {
